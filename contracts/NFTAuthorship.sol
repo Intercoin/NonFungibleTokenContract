af385a56bc0630d00435b303fa2d528b27e14fe0
Artem Subbotin <subbotin.artem@gmail.com>
2021-06-22 14:17:19 +0300

    ERc721EnumerableUpgradeble support

--- contracts/NFTAuthorship.sol
+++ contracts/NFTAuthorship.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 //import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 
-abstract contract NFTAuthorship is /*ContextUpgradeable, */ERC721URIStorageUpgradeable {
+abstract contract NFTAuthorship is /*ContextUpgradeable, */ERC721URIStorageUpgradeable, ERC721EnumerableUpgradeable {
 
     function __NFTAuthorship_init(
         string memory name,
@@ -23,8 +24,8 @@ abstract contract NFTAuthorship is /*ContextUpgradeable, */ERC721URIStorageUpgra
     
     event TransferAuthorship(address indexed from, address indexed to, uint256 indexed tokenId);
     
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        return super.tokenURI(tokenId);
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+        return ERC721URIStorageUpgradeable.tokenURI(tokenId);
     }
     
     function transferAuthorship(
@@ -55,11 +56,23 @@ abstract contract NFTAuthorship is /*ContextUpgradeable, */ERC721URIStorageUpgra
         
     }
     
-    function _burn(uint256 tokenId) internal virtual override {
+    function _burn(uint256 tokenId) internal virtual override(ERC721URIStorageUpgradeable, ERC721Upgradeable) {
         super._burn(tokenId);
         delete _authors[tokenId];
 
     }
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721EnumerableUpgradeable, ERC721Upgradeable) returns (bool) {
+        return interfaceId == type(IERC721EnumerableUpgradeable).interfaceId
+            || super.supportsInterface(interfaceId);
+    }
+
+    
     
     function _setAuthor(uint256 tokenId, address addr) private {
         _authors[tokenId] = addr;
@@ -67,4 +80,5 @@ abstract contract NFTAuthorship is /*ContextUpgradeable, */ERC721URIStorageUpgra
     function _getAuthor(uint256 tokenId) private view returns (address)  {
         return _authors[tokenId];
     }
+    
 }
