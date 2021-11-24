d3628c50ed73423436905206eb580557235548b9
Artem Subbotin <subbotin.artem@gmail.com>
2021-11-24 12:06:59 +0200

    code updated

--- contracts/mock/NFTSeriesMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "../NFTSeries.sol";
-
-contract NFTSeriesMock is NFTSeries {
-    
-    
-}
--- test/nft-bytecode.js
+++ test/nft-bytecode.js
@@ -16,17 +16,3 @@ contract('NFT', function(accounts) {
     
   });
 });
-contract('NFTSeries', function(accounts) {
-  it("get the size of the contract", async () => {
-      let instance = await NFTSeries.new();
-    
-      var bytecode = instance.constructor._json.bytecode;
-      var deployed = instance.constructor._json.deployedBytecode;
-      var sizeOfB  = bytecode.length / 2;
-      var sizeOfD  = deployed.length / 2;
-      console.log("size of bytecode in bytes = ", sizeOfB);
-      console.log("size of deployed in bytes = ", sizeOfD);
-      console.log("initialisation and constructor code in bytes = ", sizeOfB - sizeOfD);
-    
-  });
-});
