8c8c9cb2da378f28f4c4598624402d5a8e350c41
Artem Subbotin <subbotin.artem@gmail.com>
2021-12-22 15:50:57 +0200

    assets

--- arguments.js
+++ arguments.js
@@ -1,8 +1,8 @@
 module.exports = 
     [
         "0x78613Cc32ecA27DEC6f42BC08990CDB06D6b4Df6",
-        "NFT Video Test RINKEBY",
-        "NFTVTR",
+        "NFT Video Test Matic 2212",
+        "NFTVTR-2212",
         "https://pastebin.com/raw/armzdJZr",
         "0x0000000000000000000000000000000000000000"
     ]
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,8 @@
 async function main() {
 
-	const [deployer] = await ethers.getSigners();
+	//const [deployer] = await ethers.getSigners();
+	const [,deployer] = await ethers.getSigners();
+	
 	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 	console.log(
 	"Deploying contracts with the account:",
@@ -12,12 +14,12 @@ async function main() {
 	const FactoryFactory = await ethers.getContractFactory("Factory");
 	const NftFactory = await ethers.getContractFactory("NFTSafeHook");
 
-	this.nft = await NftFactory.deploy({gasLimit: 15e6});
+	this.nft = await NftFactory.deploy({gasLimit: 8e6});
 
 	const name = "NFT Video Test MATIC";
 	const symbol = "NFTVTM";
 	const contractURI = "https://pastebin.com/raw/armzdJZr";
-	this.factory = await FactoryFactory.deploy(this.nft.address, name, symbol, contractURI, ZERO_ADDRESS, {gasLimit: 15e6});
+	this.factory = await FactoryFactory.deploy(this.nft.address, name, symbol, contractURI, ZERO_ADDRESS, {gasLimit: 8e6});
 
 
 	console.log("NFT deployed at:", this.nft.address);
