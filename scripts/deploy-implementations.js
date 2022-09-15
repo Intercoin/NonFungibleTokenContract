b58f3ed3af6eeac08a777da97b145840d15d1401
Artem Subbotin <subbotin.artem@gmail.com>
2022-09-15 14:00:11 +0300

    fxies

--- scripts/deploy-implementations.js
+++ scripts/deploy-implementations.js
@@ -47,7 +47,7 @@ async function main() {
 	const [deployer] = await ethers.getSigners();
 	
 	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-    const RELEASE_MANAGER = process.env.RELEASEMANAGER;
+    const RELEASE_MANAGER = process.env.RELEASE_MANAGER;
 	console.log(
 		"Deploying contracts with the account:",
 		deployer.address
