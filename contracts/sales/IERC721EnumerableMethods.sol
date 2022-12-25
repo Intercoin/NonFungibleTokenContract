345bb22d8df6bc2d80577c493d9355113712cac5
Gregory Magarshak <greg@qbix.com>
2022-12-25 14:25:08 -0500

    Renamed IERC721 to IERC721EnumerableMethods

--- contracts/sales/IERC721EnumerableMethods.sol
+++ contracts/sales/IERC721EnumerableMethods.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.11;
 /**
  * @dev Required interface of an ERC721 compliant contract.
  */
-interface IERC721 {
+interface IERC721EnumerableMethods {
     function ownerOf(uint256 tokenId) external view returns (address owner);
     function balanceOf(address owner) external view returns (uint256 balance);
     function tokenOfOwnerByIndex(address owner, uint256 index);
