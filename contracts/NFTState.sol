52105bdb6c5dda0ed7d9379b37e6607b447df661
Gregory Magarshak <greg@qbix.com>
2023-03-16 00:01:34 -0400

    Shortened the loop

--- contracts/NFTState.sol
+++ contracts/NFTState.sol
@@ -140,8 +140,8 @@ contract NFTState is NFTStorage, INFTState {
         if (seriesInfo[forkedSeriesId].author != address(0)) {
             revert ForkAlreadyExists(); 
         }
-        uint8 p = 0;
-        for (uint8 i = 7*8; i > 0; i -= 8) {
+        uint8 p = 8;
+        for (uint8 i = 7*8; i > 8; i -= 8) {
             if (seriesId >> i << i == seriesId) {
 	    	p = i;
                 break;
